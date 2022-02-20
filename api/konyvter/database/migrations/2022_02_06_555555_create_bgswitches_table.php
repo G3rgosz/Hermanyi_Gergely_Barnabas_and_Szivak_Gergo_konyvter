35717ce479be5c1ea66ceb61696cf71599db25b9
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2022-02-20 17:13:53 +0100

    adatbázis módosítva, Bgswitch model létrehozva

--- api/konyvter/database/migrations/2022_02_06_555555_create_bgswitches_table.php
+++ api/konyvter/database/migrations/2022_02_06_555555_create_bgswitches_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBgTable extends Migration
+class CreateBgswitchesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBgTable extends Migration
      */
     public function up()
     {
-        Schema::create('bg', function (Blueprint $table) {
+        Schema::create('bgswitches', function (Blueprint $table) {
             $table->foreignId('bid')->constrained('books');
             $table->foreignId('gid')->constrained('genres');
         });
@@ -26,6 +26,6 @@ class CreateBgTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('bg');
+        Schema::dropIfExists('bgswitches');
     }
 }
