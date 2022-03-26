15b435b4dc135c7877ae84533f8f1ef1c875bae9
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2022-03-26 16:52:04 +0100

    2 új útvonal, felhasználó saját hirdetései, műfajok mostantól lekérhetőek, de továbbra sem módosíthatóak

--- api/konyvter/routes/api.php
+++ api/konyvter/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AccountController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\AdvertisementController;
+use App\Http\Controllers\GenreController;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,6 +36,7 @@ Route::group(["middleware" => ["auth:sanctum"]], function(){
     Route::put("/web/books/{id}", [BookController::class, "update"]);
     Route::delete("/web/books/{id}", [BookController::class, "delete"]);
 
+    Route::get("/web/advertisements/my", [AdvertisementController::class, "getMyAds"]);
     Route::post("/web/advertisements", [AdvertisementController::class, "create"]);
     Route::put("/web/advertisements/{id}", [AdvertisementController::class, "update"]);
     Route::put("/web/advertisements/report/{id}", [AdvertisementController::class, "reportAd"]);
@@ -45,6 +47,8 @@ Route::post("/login", [AuthController::class, "login"]);
 
 Route::get("/web/books", [BookController::class, "index"]);
 Route::get("/web/books/{id}", [BookController::class, "show"]);
+
+Route::get("/web/genres", [GenreController::class, "index"]);
 
 Route::get("/web/advertisements", [AdvertisementController::class, "index"]);
 Route::get("/web/advertisements/{id}", [AdvertisementController::class, "show"]);
