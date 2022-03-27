546e5cbc295f48f74e72c5043613fa9bce9176fd
Gergosz <71034104+Gergosz-2000@users.noreply.github.com>
2022-03-27 14:34:14 +0200

    a hirdetésfeladásnál a könyv már lérejön a hirdetés még nem

--- web/konyvter/src/app/newadvertisement/newadvertisement.component.ts
+++ web/konyvter/src/app/newadvertisement/newadvertisement.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { NewadvertisementService } from '../shared/newadvertisement.service';
 import { Router } from '@angular/router';
-import { Validators, FormBuilder, FormArray, FormControl, FormGroup} from '@angular/forms';
+import { Validators, FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-newadvertisement',
@@ -13,26 +14,24 @@ export class NewadvertisementComponent implements OnInit {
   genres:any;
   newadForm !: FormGroup;
   genreList: string[] =[];
-  file: File | null = null;
+  // file: File | null = null;
+  bookid:any;
+  imageSrc: string = "";
+  host = 'http://localhost:8000/api/';
 
   constructor(
-    private newAdS: NewadvertisementService,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private auth: AuthService
   ) { 
-    this.newadForm = this.formBuilder.group({
-
-      website: this.formBuilder.array([], [Validators.required])
-
-    })
   }
 
   ngOnInit(): void {
-    this.newAdS.getGenres()
+    this.getGenres()
     .subscribe(
       (res) => {
         this.genres = res.data;
-        console.log(this.genres);
       }, (error) => {
         console.error(error);
     });
@@ -50,20 +49,36 @@ export class NewadvertisementComponent implements OnInit {
     });
   }
   newAdvertisement(){
-    
-    let title = this.newadForm.value.title;
-    let writer = this.newadForm.value.writer;
-    let publisher = this.newadForm.value.publisher;
-    let release = this.newadForm.value.release;
-    let language = this.newadForm.value.language;
-    let genres = this.genreList;
-    let adtitle = this.newadForm.value.adtitle;
-    let price = this.newadForm.value.price;
-    let image = this.file;
-    let description = this.newadForm.value.description;
-    console.log(title,writer,publisher,release,language,genres,adtitle,price,image,description);
+    if(this.genreList.length == 0){
+      alert("Válasszon ki legalább egy műfajt!")
+    }else{
+      let bookData = {
+        title: this.newadForm.value.title,
+        writer: this.newadForm.value.writer,
+        publisher: this.newadForm.value.publisher,
+        release: this.newadForm.value.release,
+        language: this.newadForm.value.language,
+        genres: this.genreList
+      }
+
+      this.newBook(bookData)
+      .subscribe(
+        (res) => {
+          this.bookid = res.data;
+          alert("Könyv sikeresen felvéve!");
+        }, (error) => {
+          console.error(error);
+        })
+      this.newAd()
+      .subscribe(
+        (res) => {
+          alert("Hirdetés sikeresen felvéve!");
+        }, (error) => {
+          console.error(error);
+        })
+    }
   }
-  public checkboxController(genre:string){
+  checkboxController(genre:string){
     if(this.genreList.includes(genre)){
       let index = this.genreList.indexOf(genre);
       this.genreList.splice(index, 1);
@@ -72,6 +87,56 @@ export class NewadvertisementComponent implements OnInit {
     }
   }
   handleFileInput(event: any) {
-    this.file = event.target.files[0]
-}
+    let file = event.target.files[0]
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      this.imageSrc = reader.result as string;
+    }
+  }
+  getGenres(){
+    let endpoint = 'web/genres';
+    let url = this.host + endpoint;
+    return this.http.get<any>(url);
+  }
+  newBook(bookData:any){
+    let endpoint = 'web/books';
+    let url = this.host + endpoint;
+
+    let token = this.auth.isLoggedIn();
+    
+    let data = JSON.stringify(bookData);
+    let headerObj = new HttpHeaders({
+      'Content-Type':'application/json',
+      'Authorization': `Bearer ${token}`
+    })
+    let header = {
+      headers: headerObj
+    }
+    return this.http.post<any>(url,data,header);
+  }
+  newAd(){
+    let endpoint = 'api/web/advertisements';
+    let url = this.host + endpoint;
+
+    const formData: any = new FormData();
+    formData.append("adtitle", this.newadForm.value.adtitle);
+    formData.append("price", this.newadForm.value.price);
+    formData.append("description", this.newadForm.value.description);
+    formData.append("book_id", this.bookid);
+    formData.append("image", this.imageSrc); 
+
+    let token = this.auth.isLoggedIn();
+
+    let headerObj = new HttpHeaders({
+      'Content-Type':'multipart/form-data; boundary=---011000010111000001101001',
+      'Authorization': `Bearer ${token}`
+    })
+    let header = {
+      headers: headerObj
+    }
+    console.log(url);
+    console.log(formData);
+    return this.http.post<any>(url,formData,header);
+  }
 }
